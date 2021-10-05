9d4693633621fd0a89e4e07fed8a3454f4014de8
Eduardo Winter <eduardo.wbess@outlook.commit>
2021-10-05 11:19:27 -0300

    Swagger documentation initial config

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { router } from "./routes";
+import swaggerfile from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerfile));
 
 app.use(router);
 
