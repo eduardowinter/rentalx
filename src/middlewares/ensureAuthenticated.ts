561edcf9f4a5ef61479df3d723ae58b7d5e6ba52
Eduardo Winter <eduardo.wbess@outlook.commit>
2021-10-14 12:29:01 -0300

    added users and authentication

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from "express";
+
+export async function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new Error("Token missing");
+  }
+}
---
+++ src/routes/authenticate.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+import { AuthenticateUserController } from "../modules/accounts/useCases/authenticateUser/AuthenticateUserController";
+
+const authenticateRoutes = Router();
+
+const authenticateUsersController = new AuthenticateUserController();
+
+authenticateRoutes.post("/sessions", authenticateUsersController.handle);
+
+export { authenticateRoutes };
