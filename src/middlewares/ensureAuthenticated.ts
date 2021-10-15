2a166ac98e53d1b155671748c48f1fd56b799f8c
Eduardo Winter <eduardo.wbess@outlook.commit>
2021-10-15 10:40:50 -0300

    added avatar upload

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,4 +1,12 @@
 import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+import { AppError } from "../errors/AppError";
+import { UsersRepository } from "../modules/accounts/repositories/implementations/UsersRepository";
+
+interface IPayload {
+  sub: string;
+}
 
 export async function ensureAuthenticated(
   request: Request,
@@ -8,6 +16,30 @@ export async function ensureAuthenticated(
   const authHeader = request.headers.authorization;
 
   if (!authHeader) {
-    throw new Error("Token missing");
+    throw new AppError("Token missing", 401);
+  }
+
+  const [, token] = authHeader.split(" ");
+
+  try {
+    const { sub: user_id } = verify(
+      token,
+      "2434125366931e107a0921889a436d88"
+    ) as IPayload;
+
+    const usersRepository = new UsersRepository();
+    const user = await usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError("User does not exists!", 401);
+    }
+
+    request.user = {
+      id: user_id,
+    };
+
+    next();
+  } catch {
+    throw new AppError("Invalid token", 401);
   }
 }
